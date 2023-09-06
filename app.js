89f477e0264a0857ce8667498d37b58af835324d
ninjadev <ninjadev333@gmail.com>
2023-09-06 15:44:43 -0500

    connected to db with mongoose

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1/bookthingapp");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
---
+++ models/book.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: String,
+  author: String,
+  description: String,
+  year: String,
+  genre: Array,
+  avgRating: String,
+  image: String,
+  series: String,
+  pages: String,
+  awards: Array,
+  liked: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Book", BookSchema);
