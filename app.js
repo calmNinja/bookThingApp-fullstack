403fa9c4b1338a5ca223e3f78d9f8789181360ab
ninjadev <ninjadev333@gmail.com>
2023-09-07 10:39:43 -0500

    added ejs boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
 const Book = require("./models/book");
 
 mongoose.connect("mongodb://127.0.0.1/bookthingapp");
@@ -12,9 +14,11 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
