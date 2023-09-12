331f6857e1c61f2c683a333c7d92503b288d46f1
ninjadev <ninjadev333@gmail.com>
2023-09-11 21:41:40 -0500

    basic Express Error handling

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const Book = require("./models/book");
 
 mongoose.connect("mongodb://127.0.0.1/bookthingapp");
@@ -25,16 +27,32 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/books", async (req, res) => {
-  const books = await Book.find({});
-  res.render("books/index", { books });
+app.get(
+  "/books",
+  catchAsync(async (req, res) => {
+    const books = await Book.find({});
+    res.render("books/index", { books });
+  })
+);
+
+app.get(
+  "/books/:id",
+  catchAsync(async (req, res) => {
+    const book = await Book.findById(req.params.id);
+    res.render("books/show", { book });
+  })
+);
+
+//Express Error Handling
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
-app.get("/books/:id", async (req, res) => {
-  const book = await Book.findById(req.params.id);
-  res.render("books/show", { book });
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Sorry but Something went Wrong!!";
+  res.status(statusCode).render("error", { err });
 });
-
 app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
