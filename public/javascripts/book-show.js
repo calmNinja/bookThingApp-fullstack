cb797c1d39b2d53422270b5851f0d82e8b1ed1b5
ninjadev <ninjadev333@gmail.com>
2023-11-06 14:03:26 -0600

    readmorebtn made responsive

--- public/javascripts/book-show.js
+++ public/javascripts/book-show.js
@@ -1,33 +1,51 @@
 const expandableContent = document.getElementById("expandable-content");
 const readMoreBtn = document.getElementById("read-more-btn");
-const maxCharacters = 846;
+let maxCharacters;
 
-// Display the text length
+function setMaxCharacters() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth <= 480) return 200;
+  if (screenWidth <= 768) return 400;
+  if (screenWidth <= 1024) return 600;
+  return 846;
+}
+maxCharacters = setMaxCharacters();
+
 const content = expandableContent.textContent;
-const characterCount = content.length;
-console.log("Character count: " + characterCount);
 
-// If the content has less characters than the max character count, hide the button
-if (expandableContent.textContent.length <= maxCharacters) {
-  readMoreBtn.style.display = "none";
-} else {
-  // If the content has more characters than the max character count, trim it
-  const trimmedContent = content.slice(0, maxCharacters) + " ...";
-  expandableContent.textContent = trimmedContent;
+//Toggle button text and expand or shrink content accordingly
+function trimAndtoggle() {
+  const shouldExpand = !expandableContent.classList.contains("expanded");
+
+  if (shouldExpand || content.length > maxCharacters) {
+    const trimmedContent = shouldExpand
+      ? content
+      : content.slice(0, maxCharacters) + " ...";
+
+    expandableContent.textContent = trimmedContent;
+    expandableContent.classList.toggle("expanded");
+    readMoreBtn.textContent = shouldExpand ? "Read Less" : "Read More";
+  }
 }
 
+function updateContent() {
+  // If the content has less characters than the max character count, hide the button
+  if (content.length <= maxCharacters) {
+    readMoreBtn.style.display = "none";
+  } else {
+    // If the content has more characters than the max character count, trim it
+    readMoreBtn.style.display = "block";
+    trimAndtoggle();
+  }
+}
+updateContent();
+
+window.addEventListener("resize", function () {
+  maxCharacters = setMaxCharacters();
+  updateContent();
+  trimAndtoggle();
+});
 readMoreBtn.addEventListener("click", function (e) {
   e.preventDefault();
-
-  if (expandableContent.classList.contains("expanded")) {
-    // If the content is expanded, trim it again
-    const trimmedContent = content.slice(0, maxCharacters) + " ...";
-    expandableContent.textContent = trimmedContent;
-    expandableContent.classList.remove("expanded");
-    readMoreBtn.textContent = "Read More";
-  } else {
-    expandableContent.textContent = content; // Restore the full content
-    expandableContent.classList.add("expanded");
-    readMoreBtn.textContent = "Read Less";
-  }
+  trimAndtoggle();
 });
